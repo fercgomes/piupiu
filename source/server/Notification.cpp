58af6247c23007fec60e6519f2bbf49ee4d6faeb
cgsrjunior <cgsrjunior@inf.ufrgs.br>
2022-03-15 23:02:05 -0300

    Notification draw

--- source/server/Notification.cpp
+++ source/server/Notification.cpp
@@ -1,15 +1,54 @@
 #pragma once
 #include <cstdint>
+#include <ctime>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <inttypes.h>
 #include <vector>
+#include "Notification.hpp"
 #include "ProfileManager.hpp"
 
+int id_count = 0;
+Notification NotificationList;
 
 
-int NotificationPush()
+//Recebe uma lista de perfis
+//Retorna uma estrutura de notificacao
+Notification FeedNotificationList(Profile* user_profile, std::string message_sent)
 {
+    //Pick up the timestamp
+    time_t now = time(0);
+
+    //This is hipotetical to create a id fetch
+    if(id_count == 0)
+    {
+        NotificationList.id = 0;
+        NotificationList.timestamp = now;
+        NotificationList.profile = *user_profile;
+        NotificationList.message = message_sent;
+        NotificationList.sentCount = 0; //Nao sei o que esse valor deveria significar na pratica
+        //Aqui vai ter que ter um laco para percorrer a lista de seguidores
+        //Pegar o ip de cada um
+        //e colocar no vetor targets
+        for (Profile x :  user_profile.following)
+        {
+            NotificationList
+        }
+    }
+    else
+    {
+
+    }
+
+
+    //We need to fetch a profile to mount a row to do a line input
+    //Here we not focus in pick up the IP, just to create a notification struct
+
+
+
+
+
+/*
     time_t timeout = time(NULL);
     vector<string> ip_list;
 
@@ -24,6 +63,7 @@ int NotificationPush()
             }
         }
     }
+*/
 }
 
     
