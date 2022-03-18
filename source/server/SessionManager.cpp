fa7bed275ed5f44f72d085fbaa11cfac0fd1c0fa
Simon, Giovani <gio.simon@hotmail.com>
2022-03-17 21:52:50 -0300

    fixed indices on getUserNameByAddressAndIP

--- source/server/SessionManager.cpp
+++ source/server/SessionManager.cpp
@@ -92,7 +92,7 @@ std::string SessionManager::GetUserNameByAddressAndIP(in_addr address, int port)
     {
         //TODO: need to check all elements on connectedPeers
         if (it->second->connectedPeers[0].sin_port == port && it->second->connectedPeers[0].sin_addr.s_addr == address.s_addr
-        || it->second->connectedPeers[0].sin_port == port && it->second->connectedPeers[0].sin_addr.s_addr == address.s_addr)
+        || it->second->connectedPeers[1].sin_port == port && it->second->connectedPeers[1].sin_addr.s_addr == address.s_addr)
             return it -> first;
     }
 }
