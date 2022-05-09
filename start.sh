dd4b768e35cb1d445c1b0a3355bcc55a20ea18e2
Simon, Giovani <gio.simon@hotmail.com>
2022-05-09 20:31:14 -0300

    parece ta funcionando. vamoo

--- start.sh
+++ start.sh
@@ -1,28 +1,26 @@
-# Primary 127.0.0.1:5000
-PEERS=127.0.0.1:5001:s,127.0.0.1:5002:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5000 PRIMARY=true ./bin/Server
+# # Primary 127.0.0.1:5000
+# PEERS=127.0.0.1:5001:s,127.0.0.1:5002:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5000 PRIMARY=true ./bin/Server
 
-# Secondary 1 127.0.0.1:5001
-PEERS=127.0.0.1:5000:p,127.0.0.1:5002:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5001 PRIMARY=false ./bin/Server
+# # Secondary 1 127.0.0.1:5001
+# PEERS=127.0.0.1:5000:p,127.0.0.1:5002:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5001 PRIMARY=false ./bin/Server
 
-# Secondary 2 127.0.0.1:5002
-PEERS=127.0.0.1:5000:p,127.0.0.1:5001:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5002 PRIMARY=false ./bin/Server
+# # Secondary 2 127.0.0.1:5002
+# PEERS=127.0.0.1:5000:p,127.0.0.1:5001:s,127.0.0.1:5003:s BIND_IP=0.0.0.0 BIND_PORT=5002 PRIMARY=false ./bin/Server
 
-# Secondary 3 127.0.0.1:5003
-PEERS=127.0.0.1:5000:p,127.0.0.1:5001:s,127.0.0.1:5002:s BIND_IP=0.0.0.0 BIND_PORT=5003 PRIMARY=false ./bin/Server
+# # Secondary 3 127.0.0.1:5003
+# PEERS=127.0.0.1:5000:p,127.0.0.1:5001:s,127.0.0.1:5002:s BIND_IP=0.0.0.0 BIND_PORT=5003 PRIMARY=false ./bin/Server
 
 
-# Primary 127.0.0.1:6000
-PEERS=127.0.0.1:6001:s BIND_IP=0.0.0.0 BIND_PORT=6000 PRIMARY=true ./bin/Server
+# # Primary 127.0.0.1:6000
+# PEERS=127.0.0.1:6001:s BIND_IP=0.0.0.0 BIND_PORT=6000 PRIMARY=true ./bin/Server
 
-# Secondary 1 127.0.0.1:6001
+# # Secondary 1 127.0.0.1:6001
 
-valgrind --leak-check=full --track-origins=yes ./bin/Server
+# valgrind --leak-check=full --track-origins=yes ./bin/Server
 
-# Primary 127.0.0.1:5000
-PEERS=127.0.0.1:5001:s,127.0.0.1:5002:s BIND_IP=0.0.0.0 BIND_PORT=5000 PRIMARY=true ./bin/Server
 
-# Secondary 1 127.0.0.1:5001
-PEERS=127.0.0.1:5000:p,127.0.0.1:5002:s BIND_IP=0.0.0.0 BIND_PORT=5001 PRIMARY=false ./bin/Server
+#primary
+PEERS=127.0.0.1:5001:s,127.0.0.1:5000:s BIND_IP=0.0.0.0 BIND_PORT=5002 PRIMARY=true ./bin/Server
 
-# Secondary 2 127.0.0.1:5002
-PEERS=127.0.0.1:5000:p,127.0.0.1:5001:s BIND_IP=0.0.0.0 BIND_PORT=5002 PRIMARY=false ./bin/Server
+PEERS=127.0.0.1:5002:p,127.0.0.1:5000:s BIND_IP=0.0.0.0 BIND_PORT=5001 PRIMARY=false ./bin/Server
+PEERS=127.0.0.1:5002:p,127.0.0.1:5001:s BIND_IP=0.0.0.0 BIND_PORT=5000 PRIMARY=false ./bin/Server
